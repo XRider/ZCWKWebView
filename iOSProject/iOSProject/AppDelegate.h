d3ab2442b7aff3e0c587f6850de48fba7f7661a0
XRider <648554945@qq.com>
2021-05-07 14:27:41 +0800

    新增DEMO

---
+++ iOSProject/iOSProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSProject
+//
+//  Created by 肖志超 on 2021/4/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@property(nonatomic, strong) UIWindow *window;
+
+
+@end
+
